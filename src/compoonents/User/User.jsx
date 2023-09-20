7e6e12e19f0230faf02d36b95b96475f4318592f
phlabib <ph.labib@gmail.com>
2023-09-20 09:29:31 +0600

    user go back button added by Navigate on click

--- src/compoonents/User/User.jsx
+++ src/compoonents/User/User.jsx
@@ -1,20 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const User = ({user}) => {
 
     const {id, name, username, email} = user;
-    const userStyle = {
+    const navigate = useNavigate ()
+    
+    const userStyle = {    
         border: '2px solid red',
         padding : "5px",
         borderRadius : '10px',
           }
+
+          const handleShowDetails = () => {
+            navigate (`/user/${id}`);
+          }
+
     return (
         <div style={userStyle}>
             <h2>{name}</h2>
             <p>User Name : {username}</p>
             <p>Email : {email}</p>
             <Link to = {`/user/${id}`}>Show Details</Link>
+            <button onClick={handleShowDetails}>Show Details</button>
 
         </div>
     );
--- src/compoonents/UserDetails/UserDetails.jsx
+++ src/compoonents/UserDetails/UserDetails.jsx
@@ -1,13 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const UserDetails = () => {
     const user = useLoaderData ();
+    const navigate = useNavigate();
     const {name, website} = user;
+
+    const haldleGoBack = () =>{
+        navigate(-1);
+    }
     return (
         <div>
             <h2>Details About Users : {name}</h2>
             <p>WebSite : {website}</p>
+            <button onClick={haldleGoBack}>Go Back</button>
         </div>
     );
 };
